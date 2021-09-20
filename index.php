f76fc2819f1e06da95873b379ed8524e956b7348
HoangAnh <90691318+HoanggAnh@users.noreply.github.com>
2021-09-20 09:43:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wowFood</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- HEADER -->
+    <section class="nav">
+        <div class="container">
+            <div class="logo">
+                <img src="images/logo.png" alt="restaurant logo" class="img-res">
+
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li><a href="">Home</a></li> 
+                    <li><a href="">Categories</a></li>
+                    <li><a href="">Foods</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div>
+    </section>
+
+    <!-- FOOD SEARCH  -->
+    <section class="food-search text-center">
+        <div class="container">
+            <form action="">
+                <input type="search" name="search" placeholder="Search for Food..." Class="btn">
+                <input type="submit" name="submit" values="Search" Class="btn btn-primary">
+            </form>
+            
+        </div>
+    </section>
+
+    <!-- FOOD MENU -->
+    <section class="food-menu">
+        <div class="container">
+            <h2 class="text-center">Food Menu</h2>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="Chicken Hawai Pizza" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Chicken Pizza</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-burger.jpg" alt="Smoky Burger" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Smokey Burger</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-burger.jpg" alt="Smoky Burger" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Smokey Burger</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt="Beef Pizza" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Beef Pizza</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-pizza.jpg" alt=" Chicken BBQ Pizza" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Chicken BBQ Pizza</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    <img src="images/menu-momo.jpg" alt="Chicken Steam Momo" class="img-res img-curve">
+                </div>
+                <div class="food-menu-desc">
+                    <h4>Chicken Steam Momo</h4>
+                    <p class="Food-price">$2.3</p>
+                    <p class="food-detail">Made with Italian Sauce, Chicken, and organice vegetbles</p>
+                    <br>
+                    <a href="" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <p class="text-center text-last"><a href="">See All Foods</a></p>
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+  <section class="footer">
+        <div class="container">
+        <div class="container icon text-center">
+            <div class="icon">
+            <a href=""><img src="images/icon_f.png" alt="Facebook" class="img-icon"></a>
+            <a href=""><img src="images/icon_i.png" alt="Instagram" class="img-icon"></a>
+            <a href=""><img src="images/icon_t.png" alt="Twitter" class="img-icon"></a>
+            </div>
+            <p class="text-center">All rights reserved. Designed By <a href="">HoangAnh</a></p>
+        </div>
+        
+    </section>
+    
+</body>
+</html>
